5aaf515013b3acd2137018b361960fb7817f7840
khoinguyen <khoi.nguyen@kyanon.digital>
2025-11-24 13:26:18 +0700

    [feat] add desc

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -444,6 +444,7 @@ export interface ApiLoggerLogger extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    Description: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
